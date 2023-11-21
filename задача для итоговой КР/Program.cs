bd756273cbf8d511bee13a0108bcd457d2ad3012
Ognehodets <iliuie@yandex.by>
2023-11-21 18:54:07 +0300

    добавлено решение задачи на языке С#

---
+++ задача для итоговой КР/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+void PrintArray(string[] a)
+{
+    for (int i = 0; i < a.Length; i++)
+    {
+        Console.Write($"{a[i]}\t");
+    }
+    Console.WriteLine();
+}
+
+Console.Clear();
+string[] initArr = { "a", "asdfdf", "brerjf", "df", "dgd", "h", "hyuk" };
+Console.Write("Исходный массив: ");
+PrintArray(initArr);
+string[] finArr = new string[initArr.Length];
+int j = 0;
+for (int i = 0; i < initArr.Length; i++)
+{
+    if (initArr[i].Length < 4)
+    {
+        finArr[j] = initArr[i];
+        j++;
+    }
+}
+Array.Resize(ref finArr, j);
+Console.Write("Измененный массив: ");
+PrintArray(finArr);
